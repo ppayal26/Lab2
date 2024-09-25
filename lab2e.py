24d49adfbba8f6f5b578395e484ada24669cc076
Payal Payal <ppayal1@myseneca.ca>
2024-09-25 12:49:00 -0400

    lab2e added

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Your Name
+# Author ID: Your Seneca ID
+# Date Created: YYYY/MM/DD
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
